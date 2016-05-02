000543ef1334bb9f992f4ff8cdc5e25b92aba024
Victor Hugo Erminpour <victorhugoerminpour@gmail.com>
2016-05-01 23:17:01 -0700

    jump-table.

--- C_unix/jumptable.c
+++ C_unix/jumptable.c
@@ -1,13 +1,36 @@
 
 /*
- *
  * jumptable.c
  * Victor Hugo Erminpour
  */
 
-int
-main(int argc, char *argv[])
+#include <stdio.h>
+
+void foo_func(void)
 {
+    fprintf(stdout, "%s\n", __FUNCTION__);
+}
+
+void bar_func(void)
+{
+    fprintf(stdout, "%s\n", __FUNCTION__);
+}
+
+void baz_func(void)
+{
+    fprintf(stdout, "%s\n", __FUNCTION__);
+}
+
+typedef void (*Handler)(void);
+Handler jump_table[] = { foo_func, bar_func };
+
+int main(int argc, char *argv[])
+{
+
+    jump_table[0]();
+    jump_table[1]();
+    jump_table[1] = baz_func;
+    jump_table[1]();
 
 return 0;
 }
