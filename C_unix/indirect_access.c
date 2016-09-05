f048f97c7c2496e8b78c85c248bf0f6cf543ecb2
Victor Hugo Erminpour <victorhugoerminpour@gmail.com>
2016-09-04 18:57:35 -0700

    Add pointer address.

--- C_unix/indirect_access.c
+++ C_unix/indirect_access.c
@@ -7,15 +7,20 @@ void incr(int *ptr)
 {
     //*ptr = *ptr + 1;
     (*ptr)++;
+    fprintf(stdout, "%s: Address of &ptr = %p\n", __FUNCTION__, &ptr);
+    fprintf(stdout, "%s: Value of ptr = %p\n", __FUNCTION__, ptr);
+    fprintf(stdout, "%s: Value of *ptr = %d\n", __FUNCTION__, *ptr);
 }
 
 int main (int argc, char *argv[])
 {
 
     int num = 42;
-    fprintf(stdout, "Original value of num: %d\n", num);
+    fprintf(stdout, "%s: Value of num: %d\n", __FUNCTION__, num);
+    fprintf(stdout, "%s: Address of &num = %p\n", __FUNCTION__, &num);
     incr(&num);
-    fprintf(stdout, "New value of num: %d\n", num);
+    fprintf(stdout, "%s: New value of num: %d\n", __FUNCTION__, num);
+    fprintf(stdout, "%s: Address of &num = %p\n", __FUNCTION__, &num);
 
 return 0;
 }
